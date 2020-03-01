f9399c75a7ec0a712b47388dc60c06efde2b541c
xXHachimanXx <globaldep90@gmail.com>
2020-03-01 13:36:21 -0300

    Adicionando arquivos iniciais.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css"
+
+const Header = () => (
+    <header id="main-header">
+        <img id="logo-esq" src="logo192.png"></img>
+        Aprendendo react
+        <img id="logo-dir" src="logo192.png"></img>
+    </header>
+);
+
+export default Header;
---
+++ src/pages/main/main.js
@@ -0,0 +1,7 @@
+import react from 'react';
+
+const main = () => (
+    <div id="main-div">
+
+    </div>
+);
